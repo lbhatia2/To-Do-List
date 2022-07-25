250365e18cd00135cce96df1ad1f57e00962f3b3
Lina Bhatia <lina@bhatia.org>
2022-07-25 09:38:42 -0500

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Lina Bhatia on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
