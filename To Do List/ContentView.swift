cb74256ac15c04db5a783449ac17e3ad5d4f6271
Lina Bhatia <lina@bhatia.org>
2022-07-25 09:52:51 -0500

    Set up list functionality

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["First thing", "second thing", "third thing", "fourth thing", "fifth thing"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        List{
+            ForEach(things, id: \.self) {thing in
+                Text(thing)}
+            .onMove{indices , newOffset in
+                things.move(fromOffsets: indices, toOffset: newOffset)
+            }
+            .onDelete{indexSet in
+                things.remove(atOffsets: indexSet )
+            }
+            
+        }
+        .navigationBarTitle("things", displayMode: .inline)
+        .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
